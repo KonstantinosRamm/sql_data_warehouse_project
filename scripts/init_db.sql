be73debd586a1ea20280cb620496e9164780f3dd
KonstantinosRamm <ramkwnstantinos@gmail.com>
2026-01-09 21:15:58 +0200

    Created Data ingestion scripts and database initialization

--- scripts/init_db.sql
+++ scripts/init_db.sql
@@ -1,3 +1,8 @@
+
+
+
+
+
 /*
 ===============================================================================
 Create Database,schemas and tables
@@ -7,7 +12,6 @@ WARNING
     All the data will be deleted.Proceed with caution and ensure you have backups before 
     running this script
 */
-
 DROP SCHEMA IF EXISTS bronze CASCADE;
 DROP SCHEMA IF EXISTS silver CASCADE;
 DROP SCHEMA IF EXISTS gold CASCADE;
@@ -27,34 +31,32 @@ Bronze Layer
 --------------
 */
 CREATE TABLE IF NOT EXISTS bronze.crm_cust_info(
-	cst_info_id BIGSERIAL PRIMARY KEY,
+    cst_id INTEGER ,
 	cst_key VARCHAR(50),
 	cst_firstname VARCHAR(50),
 	cst_lastname VARCHAR(50),
 	cst_marital_status VARCHAR(50),
 	cst_gndr VARCHAR(50),
-	cst_create_date DATE
+	cst_create_date TEXT --store as text and then convert to date
 );
 
 CREATE TABLE IF NOT EXISTS bronze.crm_prd_info(
-    crm_prd_id  BIGSERIAL PRIMARY KEY,
-    prd_id BIGINT,
+    prd_id INTEGER,
     prd_key VARCHAR(50),
     prd_name VARCHAR(100),
     prd_cost INTEGER,
     prd_line VARCHAR(50),
-    prd_start_dt DATE,
-    prd_end_dt DATE
+    prd_start_dt TEXT, --store as text and then convert to date
+    prd_end_dt TEXT --store as text and then convert to date
 );
 
 CREATE TABLE IF NOT EXISTS bronze.crm_sales_details(
-    sls_id BIGSERIAL PRIMARY KEY,
     sls_ord_num VARCHAR(50),
     sls_prd_key VARCHAR(50),
     sls_cust_id INTEGER,
-    sls_order_dt DATE,
-    sls_ship_dt DATE,
-    sls_due_dt DATE,
+    sls_order_dt TEXT, --store as text and then convert to date
+    sls_ship_dt TEXT, --store as text and then convert to date
+    sls_due_dt TEXT, --store as text and then convert to date
     sls_sales INTEGER,
     sls_quantity INTEGER,
     sls_price INTEGER
@@ -62,20 +64,17 @@ CREATE TABLE IF NOT EXISTS bronze.crm_sales_details(
 
 
 CREATE TABLE IF NOT EXISTS bronze.erp_cust_az1(
-    cust_id BIGSERIAL PRIMARY KEY,
     cid VARCHAR(50),
-    bdate DATE,
+    bdate TEXT, --store as text and then convert to date
     gen VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS bronze.erp_loc_a101(
-    cust_id BIGSERIAL PRIMARY KEY,
     cid VARCHAR(50),
     cntry VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS bronze.erp_px_cat_g1v2(
-    px_id BIGSERIAL PRIMARY KEY,
     ID VARCHAR(50),
     CAT VARCHAR(50),
     SUBCAT VARCHAR(50),
@@ -103,11 +102,6 @@ CREATE TABLE IF NOT EXISTS gold.placeholder(
 
 
 --
-
-
-
-
-
 
 
 
---
+++ scripts/main.sql
@@ -0,0 +1,14 @@
+/*
+===============================================================================
+Create Database,schemas and tables
+===============================================================================
+WARNING
+    Running this script will drop the entire data_warehouse database if exists already.
+    All the data will be deleted.Proceed with caution and ensure you have backups before 
+    running this script
+*/
+
+-- initialize database
+\i scripts/init_db.sql
+-- populate tables
+\i scripts/import_data.sql
